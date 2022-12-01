225c7f5d4efd6c343b316c478b3c4a3aad3d7f56
Linnea Salmimaa <linnea.salmimaa@tuni.fi>
2022-12-01 12:38:19 +0200

    unit tests for drop.js

--- test/drop.test.js
+++ test/drop.test.js
@@ -3,21 +3,66 @@ import { expect } from "chai";
 import drop from "../src/drop.js";
 
 describe("Drop from beging of array", function(){
-    it("Items get removed from beging / Positive cases", function(done){
-        var inputArray = [1, 2, 3];
+    var inputArray = [1, 2, 3];
 
-        var oneDropped = drop(inputArray);
-        var oneDropped2 = drop(inputArray, 1);
-        var twoDropped = drop(inputArray, 2);
-        var notDropped = drop(inputArray, 0);
-        var allDropped = drop(inputArray, 3);
+    describe("Items get removed from beging", function(){
 
-        expect(oneDropped).to.deep.equal([2, 3]);
-        expect(oneDropped2).to.deep.equal([2, 3]);
-        expect(twoDropped).to.deep.equal([3]);
-        expect(notDropped).to.deep.equal(inputArray);
-        expect(allDropped).to.deep.equal([]);
+        it("Drop one", function(done){
+            expect(drop(inputArray)).to.deep.equal([2, 3]);
+            expect(drop(inputArray, 1)).to.deep.equal([2, 3]);
+            done();
+        })
 
-        done();
+        it("Drop multiple", function(done){
+            expect(drop(inputArray, 2)).to.deep.equal([3]);
+            expect(drop(inputArray, 3)).to.deep.equal([]);
+            expect(drop(inputArray, 6)).to.deep.equal([]);
+            done();
+        })
+
+        it("Don't drop", function(done){
+            expect(drop(inputArray, 0)).to.deep.equal(inputArray);
+            done();
+        })
+    })
+
+    describe("Invalid parameters", function(){
+        it("Null as 1st parameter", function(done){
+            expect(drop(null)).to.throw();
+            done();
+        })
+        it("String as 1st parameter", function(done){
+            expect(drop("String")).to.throw();
+            done();
+        })
+        it("Int as 1st parameter", function(done){
+            expect(drop(123)).to.throw();
+            done();
+        })
+        it("Object as 1st parameter", function(done){
+            expect(drop({a:null})).to.throw();
+            done();
+        })
+
+        it("Null as 2st parameter", function(done){
+            expect(drop(inputArray, null)).to.throw();
+            done();
+        })
+        it("String as 2st parameter", function(done){
+            expect(drop(inputArray, "String")).to.throw();
+            done();
+        })
+        it("Array as 2st parameter", function(done){
+            expect(drop(inputArray, inputArray)).to.throw();
+            done();
+        })
+        it("Object as 2st parameter", function(done){
+            expect(drop(inputArray, {a:5})).to.throw();
+            done();
+        })
+        it("Negative 2st parameter", function(done){
+            expect(drop(inputArray, -1)).to.throw();
+            done();
+        })
     })
 })
