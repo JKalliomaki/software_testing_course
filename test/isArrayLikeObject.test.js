73f455dd89b22f6a77db71937fec97c08192a8c3
Jussi Kalliomäki <jussi.kalliomaki99@gmail.com>
2022-12-12 09:02:10 +0200

    Add tests for isArrayLikeObject() -function

--- test/isArrayLikeObject.test.js
+++ test/isArrayLikeObject.test.js
@@ -2,3 +2,23 @@
 import { expect } from "chai"
 import isArrayLikeObject from "../src/isArrayLikeObject.js"
 
+describe("isArrayLikeObject", () => {
+    it("Recognize array-like objects", () => {
+        expect(isArrayLikeObject([1, 2, 3])).equal(true)
+        expect(isArrayLikeObject([])).equal(true)
+    })
+    it("return false on regular objects", () => {
+        expect(isArrayLikeObject({})).equal(false)
+        let obj = {
+            name: "matti",
+            age: 33
+        }
+        expect(isArrayLikeObject(obj)).equal(false)
+        expect(isArrayLikeObject(null)).equal(false)
+    })
+    it("Return false on other types of values", () => {
+        expect(isArrayLikeObject(2)).equal(false)
+        expect(isArrayLikeObject("hello")).equal(false)
+        expect(isArrayLikeObject(undefined)).equal(false)
+    })
+})
